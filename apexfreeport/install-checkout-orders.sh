9725c8060bf5492a8c2d2d4027f7d35c2add8d11
Jonathan B. Roberts <jonnydreamwalker@gmail.com>
2026-07-30 15:57:56 -0500

    FreePort: thin installer for pack-order checkout routes

--- apexfreeport/install-checkout-orders.sh
+++ apexfreeport/install-checkout-orders.sh
@@ -1,1 +1,8 @@
-see-file
+#!/bin/bash
+set -e
+cd /home/ec2-user/apexfreeport
+curl -sL https://raw.githubusercontent.com/jonnydreamwalker/-jdwapexherp/main/apexfreeport/checkout-routes.js.b64 -o /tmp/cr.b64
+base64 -d /tmp/cr.b64 | gunzip > checkout-routes.js
+node --check checkout-routes.js
+curl -sL https://raw.githubusercontent.com/jonnydreamwalker/-jdwapexherp/main/apexfreeport/patch-server-rawbody.sh | bash
+echo ALL_DONE
