4f0aa6424dbce267ca37f6178d4bab76a41b97c6
Jonathan B. Roberts <jonnydreamwalker@gmail.com>
2026-08-08 20:50:16 -0500

    Cache-bust particle lizards3 (dark reveal)

--- assets/js/favicon-force.js
+++ assets/js/favicon-force.js
@@ -28,7 +28,7 @@
 
   if (!window.__apexParticleGrid && !document.querySelector('script[src*="particle-grid-bg"]')) {
     var s = document.createElement("script");
-    s.src = "https://jdwapexherp.com/assets/js/particle-grid-bg.js?v=lizards2";
+    s.src = "https://jdwapexherp.com/assets/js/particle-grid-bg.js?v=lizards3";
     s.defer = true;
     document.head.appendChild(s);
   }
